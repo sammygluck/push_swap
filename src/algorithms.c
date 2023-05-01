4b14c4dbe84341b94841178cf2fadc2d9820c83d
Sammy <infogls@yahoo.com>
2023-05-01 18:54:34 -0400

    quick commit 01/05/23 15:54

--- src/algorithms.c
+++ src/algorithms.c
@@ -21,7 +21,7 @@ void	algorithms(t_node **stack_a, t_node **stack_b)
 		return ;
 	if (stack_size == 2)
 		sa(stack_a);
-	if (stack_size > 2 && < 12)
+	if (stack_size > 2 && stack_size < 12)
 		small_stack_sort(stack_a, stack_b);
 	if (stack_size > 12)
 		radix(stack_a, stack_b);
