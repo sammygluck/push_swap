4fc50e263501f7980fd2ed8ee4ea7c8c4180f73c
Sammy Gluck <infogls@yahoo.com>
2023-05-02 19:17:48 -0700

    quick

--- src/9_parsing_utils.c
+++ src/9_parsing_utils.c
@@ -90,7 +90,7 @@ int	is_actual_number(const char *str)
 
 int     is_number_within_bounds(const char *str)
 {
-        unsigned long int       number;
+        unsigned long long       number;
         int                                     i;
         int                                     sign;
 
@@ -106,7 +106,7 @@ int     is_number_within_bounds(const char *str)
         while (str[i] >= '0' && str[i] <= '9')
         {
                 number = (number * 10) + str[i] - '0';
-                if ((number > ((unsigned long int)INT_MAX + 1) && sign == -1) || (number > ((unsigned long int)INT_MAX && sign == 1)))
+                if ((number > ((unsigned long long)INT_MAX + 1))) //&& sign == -1) || (number > ((unsigned long int)INT_MAX && sign == 1)))
 				{
 					 printf("this is the number %ld \n", number);
 					 return (0);
