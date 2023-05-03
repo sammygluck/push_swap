118259e19a11964a2874fabf4393b28a68b9032f
Sammy Gluck <infogls@yahoo.com>
2023-05-02 19:14:06 -0700

    quick

--- src/9_parsing_utils.c
+++ src/9_parsing_utils.c
@@ -108,7 +108,7 @@ int     is_number_within_bounds(const char *str)
                 number = (number * 10) + str[i] - '0';
                 if ((number > ((unsigned long int)INT_MAX + 1) && sign == -1) || number > ((unsigned long int)INT_MAX && sign == 1))
 				{
-					 printf("this is the number %d \n", number);
+					 printf("this is the number %ld \n", number);
 					 return (0);
 				}
                        
